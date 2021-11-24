a91655c72b54641cecb7208cd26110e09c2dec44
Papa Bakary Camara <pcamara@redhat.com>
2021-11-24 12:45:15 +0100

    remove export NOVENV from bdd_tests/run_tests.sh script

--- bdd_tests/run_tests.sh
+++ bdd_tests/run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
 export PATH=$PATH:./
-export NOVENV=1
 
 function prepare_venv() {
     virtualenv -p python3 venv && source venv/bin/activate && python3 $(which pip3) install -r requirements.txt
