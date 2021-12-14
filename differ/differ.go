45f0c5b0cde9ef96cf66655c176210973120dbfb
Papa Bakary Camara <pcamara@redhat.com>
2021-12-14 12:50:25 +0100

    Don't pass around notifConfig struct
      - The NotificationsConfiguration struct contains 3 strings that are
        copied many many times just for the sake of retrieving these strings
        when generating the notification payload. This means that copy
        happens thousands of times in production, when the strings can be
        stored once and referenced during the execution.
      - Improvement seen for 2500 reports (locally): 0.07 seconds

--- differ/differ.go
+++ differ/differ.go
@@ -116,8 +116,11 @@ const (
 )
 
 var (
-	notificationType types.EventType
-	notifier         *producer.KafkaProducer
+	notificationType               types.EventType
+	notifier                       *producer.KafkaProducer
+	notificationClusterDetailsURI  string
+	notificationRuleDetailsURI     string
+	notificationInsightsAdvisorURL string
 )
 
 // showVersion function displays version information.
@@ -163,6 +166,10 @@ func showConfiguration(config conf.ConfigStruct) {
 		Str("Rule details URI", notificationConfig.RuleDetailsURI).
 		Msg("Notifications configuration")
 
+	notificationClusterDetailsURI = notificationConfig.ClusterDetailsURI
+	notificationRuleDetailsURI = notificationConfig.RuleDetailsURI
+	notificationInsightsAdvisorURL = notificationConfig.InsightsAdvisorURL
+
 	metricsConfig := conf.GetMetricsConfiguration(config)
 
 	// Authentication token and metrics groups values are omitted on
@@ -199,7 +206,7 @@ func findRuleByNameAndErrorKey(
 	return
 }
 
-func processReportsByCluster(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry, notificationConfig conf.NotificationsConfiguration) {
+func processReportsByCluster(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry) {
 	for i, cluster := range clusters {
 		log.Info().
 			Int("#", i).
@@ -237,7 +244,7 @@ func processReportsByCluster(ruleContent types.RulesMap, storage Storage, cluste
 		// if new report differs from the older one -> send notification
 		log.Info().Str(clusterName, string(cluster.ClusterName)).Msg("Different report from the last one")
 
-		notificationMsg := generateInstantNotificationMessage(notificationConfig.ClusterDetailsURI, fmt.Sprint(cluster.AccountNumber), string(cluster.ClusterName))
+		notificationMsg := generateInstantNotificationMessage(&notificationClusterDetailsURI, fmt.Sprint(cluster.AccountNumber), string(cluster.ClusterName))
 
 		for i, r := range deserialized.Reports {
 			module := r.Module
@@ -257,7 +264,7 @@ func processReportsByCluster(ruleContent types.RulesMap, storage Storage, cluste
 			if totalRisk >= totalRiskThreshold {
 				ReportWithHighImpact.Inc()
 				log.Warn().Int(totalRiskAttribute, totalRisk).Msg("Report with high impact detected")
-				notificationPayloadURL := generateNotificationPayloadURL(notificationConfig.RuleDetailsURI, string(cluster.ClusterName), module, errorKey)
+				notificationPayloadURL := generateNotificationPayloadURL(&notificationRuleDetailsURI, string(cluster.ClusterName), module, errorKey)
 				appendEventToNotificationMessage(notificationPayloadURL, &notificationMsg, description, totalRisk, time.Time(cluster.UpdatedAt).UTC().Format(time.RFC3339Nano))
 			}
 		}
@@ -304,7 +311,7 @@ func updateDigestNotificationCounters(digest *types.Digest, totalRisk int) {
 }
 
 // processAllReportsFromCurrentWeek function creates weekly digest with for all the clusters corresponding to each user account
-func processAllReportsFromCurrentWeek(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry, notificationConfig conf.NotificationsConfiguration) {
+func processAllReportsFromCurrentWeek(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry) {
 	digestByAccount := map[types.AccountNumber]types.Digest{}
 	digest := types.Digest{}
 
@@ -373,7 +380,7 @@ func processAllReportsFromCurrentWeek(ruleContent types.RulesMap, storage Storag
 			Int("important notifications", digest.ImportantNotifications).
 			Msg("Producing weekly notification for ")
 
-		notification := generateWeeklyNotificationMessage(notificationConfig.InsightsAdvisorURL, fmt.Sprint(account), digest)
+		notification := generateWeeklyNotificationMessage(&notificationInsightsAdvisorURL, fmt.Sprint(account), digest)
 		_, _, err := notifier.ProduceMessage(notification)
 		if err != nil {
 			log.Error().
@@ -385,13 +392,11 @@ func processAllReportsFromCurrentWeek(ruleContent types.RulesMap, storage Storag
 }
 
 // processClusters function creates desired notification messages for all the clusters obtained from the database
-func processClusters(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry, config conf.ConfigStruct) {
-	notificationConfig := conf.GetNotificationsConfiguration(config)
-
+func processClusters(ruleContent types.RulesMap, storage Storage, clusters []types.ClusterEntry) {
 	if notificationType == types.InstantNotif {
-		processReportsByCluster(ruleContent, storage, clusters, notificationConfig)
+		processReportsByCluster(ruleContent, storage, clusters)
 	} else if notificationType == types.WeeklyDigest {
-		processAllReportsFromCurrentWeek(ruleContent, storage, clusters, notificationConfig)
+		processAllReportsFromCurrentWeek(ruleContent, storage, clusters)
 	}
 }
 
@@ -420,12 +425,15 @@ func setupNotificationProducer(brokerConfig conf.KafkaConfiguration) {
 	notifier = producer
 }
 
-// generateInstantNotificationMessage function generates a notification message with no events for a given account+cluster
-func generateInstantNotificationMessage(clusterURI string, accountID string, clusterID string) (notification types.NotificationMessage) {
+// generateInstantNotificationMessage function generates a notification message
+// container with no events for a given account+cluster
+func generateInstantNotificationMessage(
+	clusterURI *string, accountID string, clusterID string) (
+	notification types.NotificationMessage) {
 	events := []types.Event{}
 	context := toJSONEscapedString(types.NotificationContext{
 		notificationContextDisplayName: clusterID,
-		notificationContextHostURL:     strings.Replace(clusterURI, "{cluster_id}", clusterID, 1),
+		notificationContextHostURL:     strings.Replace(*clusterURI, "{cluster_id}", clusterID, 1),
 	})
 	if context == "" {
 		log.Error().Msg(contextToEscapedStringError)
@@ -445,9 +453,9 @@ func generateInstantNotificationMessage(clusterURI string, accountID string, clu
 }
 
 // generateWeeklyNotificationMessage function generates a notification message with one event based on the provided digest
-func generateWeeklyNotificationMessage(advisorURI string, accountID string, digest types.Digest) (notification types.NotificationMessage) {
+func generateWeeklyNotificationMessage(advisorURI *string, accountID string, digest types.Digest) (notification types.NotificationMessage) {
 	context := toJSONEscapedString(types.NotificationContext{
-		notificationContextAdvisorURL: advisorURI,
+		notificationContextAdvisorURL: *advisorURI,
 	})
 	if context == "" {
 		log.Error().Msg(contextToEscapedStringError)
@@ -490,10 +498,12 @@ func generateWeeklyNotificationMessage(advisorURI string, accountID string, dige
 	return
 }
 
-func generateNotificationPayloadURL(ruleURI string, clusterID string, module types.ModuleName, errorKey types.ErrorKey) (notificationPayloadURL string) {
+func generateNotificationPayloadURL(
+	ruleURI *string, clusterID string, module types.ModuleName, errorKey types.ErrorKey) (
+	notificationPayloadURL string) {
 	parsedModule := strings.ReplaceAll(string(module), ".", "|")
 	replacer := strings.NewReplacer("{cluster_id}", clusterID, "{module}", parsedModule, "{error_key}", string(errorKey))
-	notificationPayloadURL = replacer.Replace(ruleURI)
+	notificationPayloadURL = replacer.Replace(*ruleURI)
 	return
 }
 
@@ -678,7 +688,7 @@ func startDiffer(config conf.ConfigStruct, storage *DBStorage) {
 	log.Info().Msg("Kafka producer ready")
 	log.Info().Msg(separator)
 	log.Info().Msg("Checking new issues for all new reports")
-	processClusters(ruleContent, storage, clusters, config)
+	processClusters(ruleContent, storage, clusters)
 	log.Info().Msg(separator)
 	closeStorage(storage)
 	log.Info().Msg(separator)
