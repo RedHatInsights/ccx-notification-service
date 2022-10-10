94ad09414c03e0e02935b4de6ff779f45bd4249a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-10 09:57:23 +0200

    Allow list benchmark

--- differ/cluster_filter_benchmark_test.go
+++ differ/cluster_filter_benchmark_test.go
@@ -591,3 +591,55 @@ func Benchmark10ClustersInBlockListFilter10000Clusters(b *testing.B) {
 	// start benchmark
 	runBenchmark(b, clusters, config)
 }
+
+// Check cluster list processing for filter with 10 known clusters and cluster
+// list with 10 clusters
+func Benchmark10ClustersInAllowListFilter10Clusters(b *testing.B) {
+	// configuration used during filtering
+	config := configurationTenAllowClustersConfig
+
+	// fill-in list of clusters at input
+	clusters := prepareListOfNClusters(10)
+
+	// start benchmark
+	runBenchmark(b, clusters, config)
+}
+
+// Check cluster list processing for filter with 10 known clusters and cluster
+// list with 100 clusters
+func Benchmark10ClustersInAllowListFilter100Clusters(b *testing.B) {
+	// configuration used during filtering
+	config := configurationTenAllowClustersConfig
+
+	// fill-in list of clusters at input
+	clusters := prepareListOfNClusters(100)
+
+	// start benchmark
+	runBenchmark(b, clusters, config)
+}
+
+// Check cluster list processing for filter with 10 known clusters and cluster
+// list with 1000
+func Benchmark10ClustersInAllowListFilter1000Clusters(b *testing.B) {
+	// configuration used during filtering
+	config := configurationTenAllowClustersConfig
+
+	// fill-in list of clusters at input
+	clusters := prepareListOfNClusters(1000)
+
+	// start benchmark
+	runBenchmark(b, clusters, config)
+}
+
+// Check cluster list processing for filter with 10 known clusters and cluster
+// list with 10000
+func Benchmark10ClustersInAllowListFilter10000Clusters(b *testing.B) {
+	// configuration used during filtering
+	config := configurationTenAllowClustersConfig
+
+	// fill-in list of clusters at input
+	clusters := prepareListOfNClusters(10000)
+
+	// start benchmark
+	runBenchmark(b, clusters, config)
+}
