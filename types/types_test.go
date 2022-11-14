11c008f8e70cb0bfe7d02118c370f5707a7214be
José Luis Segura Lucas <joseluis@redhat.com>
2022-11-14 23:24:58 +0100

    Implement tags filtering

---
+++ types/types_test.go
@@ -0,0 +1,58 @@
+// Copyright 2022 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types_test
+
+import (
+	"testing"
+
+	"github.com/RedHatInsights/ccx-notification-service/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeSetOfTags(t *testing.T) {
+	var testScenarios = []struct {
+		input    []string
+		expected types.TagsSet
+	}{
+		{
+			input:    []string{},
+			expected: map[string]struct{}{},
+		},
+		{
+			input: []string{"tag1"},
+			expected: map[string]struct{}{
+				"tag1": struct{}{},
+			},
+		},
+		{
+			input: []string{"tag1", "tag2"},
+			expected: map[string]struct{}{
+				"tag1": struct{}{},
+				"tag2": struct{}{},
+			},
+		},
+		{
+			input: []string{"tag1", "tag1"},
+			expected: map[string]struct{}{
+				"tag1": struct{}{},
+			},
+		},
+	}
+
+	for _, scenario := range testScenarios {
+		value := types.MakeSetOfTags(scenario.input)
+		assert.Equal(t, value, scenario.expected)
+	}
+}
