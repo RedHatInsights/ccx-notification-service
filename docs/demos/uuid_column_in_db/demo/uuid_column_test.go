36aa7eadd5b386be913ac2e7768002939954abb5
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-05 08:39:01 +0100

    SQL statements used during benchmarking

--- docs/demos/uuid_column_in_db/demo/uuid_column_test.go
+++ docs/demos/uuid_column_in_db/demo/uuid_column_test.go
@@ -17,7 +17,54 @@ limitations under the License.
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/lib/pq" // PostgreSQL database driver
+
 	"testing"
+)
+
+// SQL statements to create and drop tables used in benchmarks
+const (
+	CreateTableReportedBenchmarkVarcharClusterID = `
+		CREATE TABLE IF NOT EXISTS reported_benchmark_1 (
+		    org_id            integer not null,
+		    account_number    integer not null,
+		    cluster           character(36) not null,
+		    notification_type integer not null,
+		    state             integer not null,
+		    report            varchar not null,
+		    updated_at        timestamp not null,
+		    notified_at       timestamp not null,
+		    error_log         varchar,
+		                
+		    PRIMARY KEY (org_id, cluster, notified_at)
+		);
+		`
+
+	DropTableReportedBenchmarkVarcharClusterID = `
+	        DROP TABLE IF EXISTS reported_benchmark_1;
+        `
+	// Index for the reported table used in benchmarks for
+	// notified_at column
+	CreateIndexReportedNotifiedAtDescV1 = `
+                CREATE INDEX IF NOT EXISTS notified_at_desc_idx
+		    ON reported_benchmark_1
+		 USING btree (notified_at DESC);
+        `
+
+	// Insert one record into reported table
+	InsertIntoReportedV1Statement = `
+            INSERT INTO reported_benchmark_1
+            (org_id, account_number, cluster, notification_type, state, report, updated_at, notified_at, error_log)
+            VALUES
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	`
 )
 
 func BenchmarkInsertUUIDAsVarchar(b *testing.B) {
