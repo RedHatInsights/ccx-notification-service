dc1452b4d09885c737f1b61f800d9e445500277b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-05 08:37:25 +0100

    Script to run benchmark

---
+++ docs/demos/uuid_column_in_db/demo/run_benchmark.sh
@@ -0,0 +1,8 @@
+export DB_USER_NAME="psql_user"
+export DB_PASSWORD="pasword"
+export DB_HOST="localhost"
+export DB_PORT="5432"
+export DB_NAME="benchmark"
+export DB_PARAMS="sslmode=disable"
+
+go test -bench=. -benchtime=1000x
