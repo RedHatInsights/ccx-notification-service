68b7192907148cf92abf4ad17ba5fc1b12b09dfd
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-05 08:37:33 +0100

    Stub for benchmarks

---
+++ docs/demos/uuid_column_in_db/demo/uuid_column_test.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2023 Pavel Tisnovsky
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkInsertUUIDAsVarchar(b *testing.B) {
+}
+
+func BenchmarkInsertUUIDAsBytea(b *testing.B) {
+}
+
+func BenchmarkInsertUUIDAsUUID(b *testing.B) {
+}
+
+func BenchmarkDeleteUUIDAsVarchar(b *testing.B) {
+}
+
+func BenchmarkDeleteUUIDAsBytea(b *testing.B) {
+}
+
+func BenchmarkDeleteUUIDAsUUID(b *testing.B) {
+}
+
+func BenchmarkSelectUUIDAsVarchar(b *testing.B) {
+}
+
+func BenchmarkSelectUUIDAsBytea(b *testing.B) {
+}
+
+func BenchmarkSelectUUIDAsUUID(b *testing.B) {
+}
