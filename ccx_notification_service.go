459d10df2785607093f6673260bf8b344b583557
dpensi <daniele.pensiero@gmail.com>
2023-03-31 15:07:48 +0200

    don't output logs to stderr
    update config
    
    change config

--- ccx_notification_service.go
+++ ccx_notification_service.go
@@ -50,13 +50,8 @@ package main
 
 import (
 	"github.com/RedHatInsights/ccx-notification-service/differ"
-	"os"
-
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	differ.Run()
 }
