0baa65287294120ea95fbe753f9d9b9fa62c3f41
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-17 10:46:53 +0200

    Updated unit tests accordingly

--- differ/export_test.go
+++ differ/export_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020, 2021, 2022 Red Hat, Inc.
+Copyright © 2020, 2021, 2022, 2023 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ var (
 )
 
 func PushMetrics(metricsConf conf.MetricsConfiguration) {
-	pushMetrics(metricsConf)
+	pushMetrics(&metricsConf)
 }
 
 func InClauseFromStringSlice(slice []string) string {
