93d346d11f3564ad028d83b8acc0b6a64d47c4a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-17 13:47:41 +0200

    Negative test case

--- differ/storage_test.go
+++ differ/storage_test.go
@@ -1363,3 +1363,32 @@ func TestCleanupNewReports(t *testing.T) {
 	// check if all expectations were met
 	checkAllExpectations(t, mock)
 }
+
+// TestCleanupNewReportsOnError function checks the method Storage.CleanupNewReports when error is
+// detected during cleanup operation.
+func TestCleanupNewReportsOnError(t *testing.T) {
+	const cleanupStatement = "DELETE FROM new_reports WHERE updated_at < NOW\\(\\) - \\$1::INTERVAL"
+	const maxAge = "1 day"
+
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	mock.ExpectExec(cleanupStatement).WillReturnError(mockedError)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := differ.NewFromConnection(connection, 1)
+
+	// call the tested method
+	_, err := storage.CleanupNewReports(maxAge)
+	assert.Error(t, err, "error was expected while writing error report")
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
