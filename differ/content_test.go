427fe5ec2ae213f9b4c7360049c681843b394853
Jakub Drobena <jdrobena@redhat.com>
2023-10-18 13:56:44 +0200

    Quick code fix

--- differ/content_test.go
+++ differ/content_test.go
@@ -14,39 +14,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func makeFetchRequest(testFileName string) (err error, rules types.RulesMap) {
+func makeFetchRequest(testFileName string) (rules types.RulesMap, err error) {
 	config, err := conf.LoadConfiguration(configFileEnvVariableName, defaultConfigFileName)
-	if err == nil {
-		s := httptest.NewServer(http.HandlerFunc(Handler))
-		depencenciesConfiguration := conf.GetDependenciesConfiguration(&config)
-		depencenciesConfiguration.ContentServiceServer = s.URL
-		depencenciesConfiguration.ContentServiceEndpoint = "/tests/content/" + testFileName
-		rules, err = fetchAllRulesContent(&depencenciesConfiguration)
+	if err != nil {
+		return rules, err
 	}
-	return err, rules
+	server := httptest.NewServer(http.HandlerFunc(Handler))
+	depencenciesConfiguration := conf.GetDependenciesConfiguration(&config)
+	depencenciesConfiguration.ContentServiceServer = server.URL
+	depencenciesConfiguration.ContentServiceEndpoint = "/tests/content/" + testFileName
+	rules, err = fetchAllRulesContent(&depencenciesConfiguration)
+	return rules, err
 }
+
+// TestFetchAllRulesContentOk cheks if the functon fetchAllRulesContent return valid  RulesMap
 func TestFetchAllRulesContentOk(t *testing.T) {
-	err, rules := makeFetchRequest("ok")
+	rules, err := makeFetchRequest("ok")
 	assert.NoError(t, err)
 	allContentFromMap := getAllContentFromMap(rules)
 	assert.NotEmpty(t, allContentFromMap)
 	assert.Len(t, allContentFromMap, 2)
 }
 func TestFetchAllRulesContentNoInternal(t *testing.T) {
-	err, rules := makeFetchRequest("no_internal")
+	rules, err := makeFetchRequest("no_internal")
 	assert.NoError(t, err)
 	allContentFromMap := getAllContentFromMap(rules)
 	assert.NotEmpty(t, allContentFromMap)
 	assert.Len(t, allContentFromMap, 1)
 }
 func TestFetchAllRulesContentNoExternal(t *testing.T) {
-	err, rules := makeFetchRequest("no_external")
+	rules, err := makeFetchRequest("no_external")
 	assert.NoError(t, err)
 	allContentFromMap := getAllContentFromMap(rules)
 	assert.Empty(t, allContentFromMap)
 }
 func TestFetchAllRulesContentMissing(t *testing.T) {
-	err, rules := makeFetchRequest("missing")
+	rules, err := makeFetchRequest("missing")
 	assert.NoError(t, err)
 	allContentFromMap := getAllContentFromMap(rules)
 	assert.Empty(t, allContentFromMap)
@@ -57,9 +60,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	enc := gob.NewEncoder(&temp)
 	err := enc.Encode(con)
 	var tosend string
-	if err == nil {
-		tosend = temp.String()
+	if err != nil {
 		fmt.Fprint(w, tosend)
+		return
 	}
+	tosend = temp.String()
 	fmt.Fprint(w, tosend)
 }
