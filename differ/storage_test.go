9d1bd27f9dd5586f91b005a1a7dff8143527ab13
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-18 09:13:29 +0200

    Positive test case

--- differ/storage_test.go
+++ differ/storage_test.go
@@ -1506,3 +1506,31 @@ func TestDeleteRowFromNewReportsOnError(t *testing.T) {
 	// check if all expectations were met
 	checkAllExpectations(t, mock)
 }
+
+// TestDeleteRowFromReported function checks the method
+// Storage.DeleteRowFromReported.
+func TestDeleteRowFromReported(t *testing.T) {
+	const cleanupStatement = "DELETE   FROM reported  WHERE org_id = \\$1    AND cluster = \\$2    AND notified_at = \\$3"
+	const orgID = 2
+	const clusterName = "00000000-0000-0000-0000-000000000000"
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	mock.ExpectExec(cleanupStatement).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := differ.NewFromConnection(connection, 1)
+
+	// call the tested method
+	affected, err := storage.DeleteRowFromReported(types.OrgID(orgID), types.ClusterName(clusterName), types.Timestamp(time.Now()))
+	assert.Equal(t, affected, 1)
+	assert.NoError(t, err, "error was not expected while deleting one row")
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
