e3e59db830ec7d440c22f02af587dd12e3bd1bf1
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-18 12:09:36 +0200

    Negative test case: scan error

--- differ/storage_test.go
+++ differ/storage_test.go
@@ -1790,3 +1790,42 @@ func TestPrintNewReportsNonEmptyRecordSet(t *testing.T) {
 	// check if all expectations were met
 	checkAllExpectations(t, mock)
 }
+
+// TestPrintNewReportsOnScanError checks if method Storage.PrintNewReports performs
+// the test for query errors.
+func TestPrintNewReportsOnScanError(t *testing.T) {
+	const maxAge = "1 day"
+	const tableName = "foo"
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"org_id", "account_number", "cluster_name", "updated_at", "kafka_offset"})
+
+	// these three rows should be returned
+	rows.AddRow(0, "not a number!", "ID=0", time.Now(), 0)
+	rows.AddRow(1, "not a number!", "ID=1", time.Now(), 0)
+	rows.AddRow(2, "not a number!", "ID=2", time.Now(), 0)
+
+	// expected query performed by tested function
+	expectedQuery := "SELECT id, value, FROM foo ORDER BY id"
+
+	mock.ExpectQuery(expectedQuery).WillReturnRows(rows)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := differ.NewFromConnection(connection, 1)
+
+	// call the tested method
+	err := storage.PrintNewReports(maxAge, expectedQuery, tableName)
+
+	// tested method should return an error
+	assert.Error(t, err, "error was expected while querying database")
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
