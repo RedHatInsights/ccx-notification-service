e7b88ae457650055cb53f851f62f824f19a3bb86
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-19 08:02:02 +0200

    Positive test case: empty record set

--- differ/storage_test.go
+++ differ/storage_test.go
@@ -1879,3 +1879,37 @@ func TestStorageClose(t *testing.T) {
 	err = storage.Close()
 	assert.NoError(t, err, "error closing storage")
 }
+
+// TestPrintNewReportsForCleanupEmptyRecordSet checks if method Storage.PrintNewReportsForCleanup performs
+// the right query when empty set is returned.
+func TestPrintNewReportsForCleanupEmptyRecordSet(t *testing.T) {
+	const maxAge = "1 day"
+	const tableName = "foo"
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"org_id", "account_number", "cluster_name", "updated_at", "kafka_offset"})
+
+	// expected query performed by tested function
+	expectedQuery := "SELECT org_id, account_number, cluster, updated_at, kafka_offset\n\t\t  FROM new_reports\n\t\t WHERE updated_at < NOW\\(\\) - \\$1::INTERVAL\n\t\t ORDER BY updated_at\n"
+
+	mock.ExpectQuery(expectedQuery).WillReturnRows(rows)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := differ.NewFromConnection(connection, 1)
+
+	// call the tested method
+	err := storage.PrintNewReportsForCleanup(maxAge)
+
+	// tested method should NOT return an error
+	assert.NoError(t, err, "error was not expected while querying database")
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
