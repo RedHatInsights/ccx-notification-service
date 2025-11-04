f4f30da8a249142ceec1395775e4540cee90a3cc
José Luis Segura Lucas <joseluis@redhat.com>
2025-11-04 13:02:27 +0100

    Increase test coverage

--- differ/service_log_test.go
+++ differ/service_log_test.go
@@ -165,95 +165,125 @@ func TestGetServiceLogSeverityInvalidTotalRiskIsSetToInfo(t *testing.T) {
 
 // ---------------------------------------------------------------------------------------
 func TestProduceEntriesToServiceLog(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/rendered_reports" {
-			t.Errorf("Expected to request '/render_reports', got: %s", r.URL.Path)
-		}
-		if r.Header.Get("Content-Type") != "application/json" {
-			t.Errorf("Expected Content-Type: application/json header, got: %s", r.Header.Get("Accept"))
-		}
-		w.WriteHeader(http.StatusOK)
-		_, err := fmt.Fprintf(w, `{"clusters":["84f7eedc-0dd8-49cd-9d4d-f6646df3a5bc"],"reports":{"84f7eedc-0dd8-49cd-9d4d-f6646df3a5bc":[{"rule_id":"node_installer_degraded","error_key":"ek1","resolution":"rule 1 resolution","reason":"%s","description":"%s"},{"rule_id":"rule2","error_key":"ek2","resolution":"rule 2 resolution","reason":"rule 2 reason","description":"rule 2 error key description"}, {"rule_id":"rule3","error_key":"ek3","resolution":"rule 3 resolution","reason":"rule 3 reason","description":"rule 3 error key description"}]}}`, reasonTooLong, summary)
-		if err != nil {
-			log.Fatal().Msg(err.Error())
-		}
-	}))
-	defer server.Close()
-
-	config := conf.ConfigStruct{
-		ServiceLog: conf.ServiceLogConfiguration{
-			Enabled:            true,
-			TotalRiskThreshold: 1,
-			EventFilter:        "totalRisk > totalRiskThreshold",
+	testCases := []struct {
+		name                                 string
+		produceMessageError                  error
+		expectedMessages                     int
+		expectedProduceMessageCalls          int
+		expectedNotificationSentDiff         int
+		expectedNotificationNotSentErrorDiff int
+	}{
+		{
+			name:                                 "Success - all messages sent",
+			produceMessageError:                  nil,
+			expectedMessages:                     3,
+			expectedProduceMessageCalls:          3,
+			expectedNotificationSentDiff:         3,
+			expectedNotificationNotSentErrorDiff: 0,
 		},
-		Dependencies: conf.DependenciesConfiguration{
-			TemplateRendererServer:   server.URL,
-			TemplateRendererEndpoint: "/rendered_reports",
-			TemplateRendererURL:      server.URL + "/rendered_reports",
+		{
+			name:                                 "Failure - ProduceMessage returns error",
+			produceMessageError:                  fmt.Errorf("failed to produce message"),
+			expectedMessages:                     0,
+			expectedProduceMessageCalls:          3,
+			expectedNotificationSentDiff:         0,
+			expectedNotificationNotSentErrorDiff: 3,
 		},
 	}
 
-	producerMock := mocks.Producer{}
-	producerMock.On("ProduceMessage", mock.AnythingOfType("types.ProducerMessage")).Return(
-		func(msg types.ProducerMessage) int32 {
-			return 0
-		},
-		func(msg types.ProducerMessage) int64 {
-			return 0
-		},
-		func(msg types.ProducerMessage) error {
-			return nil
-		},
-	)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != "/rendered_reports" {
+					t.Errorf("Expected to request '/render_reports', got: %s", r.URL.Path)
+				}
+				if r.Header.Get("Content-Type") != "application/json" {
+					t.Errorf("Expected Content-Type: application/json header, got: %s", r.Header.Get("Accept"))
+				}
+				w.WriteHeader(http.StatusOK)
+				_, err := fmt.Fprintf(w, `{"clusters":["84f7eedc-0dd8-49cd-9d4d-f6646df3a5bc"],"reports":{"84f7eedc-0dd8-49cd-9d4d-f6646df3a5bc":[{"rule_id":"node_installer_degraded","error_key":"ek1","resolution":"rule 1 resolution","reason":"%s","description":"%s"},{"rule_id":"rule2","error_key":"ek2","resolution":"rule 2 resolution","reason":"rule 2 reason","description":"rule 2 error key description"}, {"rule_id":"rule3","error_key":"ek3","resolution":"rule 3 resolution","reason":"rule 3 reason","description":"rule 3 error key description"}]}}`, reasonTooLong, summary)
+				if err != nil {
+					log.Fatal().Msg(err.Error())
+				}
+			}))
+			defer server.Close()
 
-	d := differ.Differ{
-		NotificationType: types.InstantNotif,
-		Target:           types.NotificationBackendTarget,
-		Filter:           differ.DefaultEventFilter,
-		Notifier:         &producerMock,
+			config := conf.ConfigStruct{
+				ServiceLog: conf.ServiceLogConfiguration{
+					Enabled:            true,
+					TotalRiskThreshold: 1,
+					EventFilter:        "totalRisk > totalRiskThreshold",
+				},
+				Dependencies: conf.DependenciesConfiguration{
+					TemplateRendererServer:   server.URL,
+					TemplateRendererEndpoint: "/rendered_reports",
+					TemplateRendererURL:      server.URL + "/rendered_reports",
+				},
+			}
+
+			producerMock := mocks.Producer{}
+			producerMock.On("ProduceMessage", mock.AnythingOfType("types.ProducerMessage")).Return(
+				func(msg types.ProducerMessage) int32 {
+					return 0
+				},
+				func(msg types.ProducerMessage) int64 {
+					return 0
+				},
+				func(msg types.ProducerMessage) error {
+					return tc.produceMessageError
+				},
+			)
+
+			d := differ.Differ{
+				NotificationType: types.InstantNotif,
+				Target:           types.NotificationBackendTarget,
+				Filter:           differ.DefaultEventFilter,
+				Notifier:         &producerMock,
+			}
+
+			ruleContent := types.RulesMap{
+				"node_installer_degraded": testdata.RuleContent1,
+				"rule2":                   testdata.RuleContent2,
+				"rule3":                   testdata.RuleContent3,
+			}
+
+			rules := differ.GetAllContentFromMap(ruleContent)
+
+			cluster := types.ClusterEntry{
+				OrgID:         1,
+				AccountNumber: 1,
+				ClusterName:   types.ClusterName(testdata.ClusterName),
+				KafkaOffset:   0,
+				UpdatedAt:     types.Timestamp(testTimestamp),
+			}
+
+			var deserialized types.Report
+			err := json.Unmarshal([]byte(testdata.ClusterReport3Rules), &deserialized)
+			if err != nil {
+				log.Error().Msg(err.Error())
+				return
+			}
+			reports := deserialized.Reports
+
+			var notificationSentBefore = int(testutil.ToFloat64(differ.NotificationSent))
+			var notificationNotSentErrorStateBefore = int(testutil.ToFloat64(differ.NotificationNotSentErrorState))
+			var notificationNotSentSameStateBefore = int(testutil.ToFloat64(differ.NotificationNotSentSameState))
+
+			messages, err := d.ProduceEntriesToServiceLog(&config, cluster, rules, ruleContent, reports)
+			producerMock.AssertNumberOfCalls(t, "ProduceMessage", tc.expectedProduceMessageCalls)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedMessages, messages)
+
+			notificationSentDiff := int(testutil.ToFloat64(differ.NotificationSent)) - notificationSentBefore
+			notificationNotSentErrorStateDiff := int(testutil.ToFloat64(differ.NotificationNotSentErrorState)) - notificationNotSentErrorStateBefore
+			notificationNotSentSameStateDiff := int(testutil.ToFloat64(differ.NotificationNotSentSameState)) - notificationNotSentSameStateBefore
+
+			assert.Equal(t, tc.expectedNotificationSentDiff, notificationSentDiff,
+				"Unexpected NotificationSent metric value")
+			assert.Equal(t, tc.expectedNotificationNotSentErrorDiff, notificationNotSentErrorStateDiff,
+				"Unexpected NotificationNotSentErrorState metric value")
+			assert.Equal(t, 0, notificationNotSentSameStateDiff,
+				"Expected metric value to be 0 as the reports weren't already notified")
+		})
 	}
-
-	ruleContent := types.RulesMap{
-		"node_installer_degraded": testdata.RuleContent1,
-		"rule2":                   testdata.RuleContent2,
-		"rule3":                   testdata.RuleContent3,
-	}
-
-	rules := differ.GetAllContentFromMap(ruleContent)
-
-	cluster := types.ClusterEntry{
-		OrgID:         1,
-		AccountNumber: 1,
-		ClusterName:   types.ClusterName(testdata.ClusterName),
-		KafkaOffset:   0,
-		UpdatedAt:     types.Timestamp(testTimestamp),
-	}
-
-	var deserialized types.Report
-	err := json.Unmarshal([]byte(testdata.ClusterReport3Rules), &deserialized)
-	if err != nil {
-		log.Error().Msg(err.Error())
-		return
-	}
-	reports := deserialized.Reports
-
-	var notificationSentBefore = int(testutil.ToFloat64(differ.NotificationSent))
-	var notificationNotSentErrorStateBefore = int(testutil.ToFloat64(differ.NotificationNotSentErrorState))
-	var notificationNotSentSameStateBefore = int(testutil.ToFloat64(differ.NotificationNotSentSameState))
-
-	messages, err := d.ProduceEntriesToServiceLog(&config, cluster, rules, ruleContent, reports)
-	producerMock.AssertNumberOfCalls(t, "ProduceMessage", 3)
-	assert.Nil(t, err)
-	assert.Equal(t, 3, messages)
-
-	notificationSentDiff := int(testutil.ToFloat64(differ.NotificationSent)) - notificationSentBefore
-	notificationNotSentErrorStateDiff := int(testutil.ToFloat64(differ.NotificationNotSentErrorState)) - notificationNotSentErrorStateBefore
-	notificationNotSentSameStateDiff := int(testutil.ToFloat64(differ.NotificationNotSentSameState)) - notificationNotSentSameStateBefore
-
-	assert.Equal(t, 3, notificationSentDiff,
-		"Expected metric value to be 3 as we sent 3 messages")
-	assert.Equal(t, 0, notificationNotSentErrorStateDiff,
-		"Expected metric value to be 0 as we sent 2 messages successfully")
-	assert.Equal(t, 0, notificationNotSentSameStateDiff,
-		"Expected metric value to be 0 as the reports weren't already notified")
 }
